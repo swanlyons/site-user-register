c3115b2b884f97eb2a11b6e9b940a0224ff1bfd1
Sean Lyons <slyons494@gmail.com>
2022-05-18 14:49:11 -0500

    Initial commit, created minimal company register, log-in page.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#site-user-register setup.py 
+
+from distutils.core import setup
+
+setup(
+    name='site-user-register',
+    version='0.0.1',
+    packages=['site-user-register',],
+    license="GNU Public License",
+    long_description=open('README.txt').read(),
+    )
---
+++ website/register_app/urls.py
@@ -0,0 +1,13 @@
+#register_app urls.py
+
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView,LogoutView
+
+urlpatterns = [
+	path('', views.indexView, name='home'),
+	path('dashboard/',views.dashboardView,name='dashboard'),
+	path('login/',LoginView.as_view(),name='login_url'),
+	path('logout/',LogoutView.as_view(next_page='dashboard'),name='logout'),
+	path('register/',views.registerView,name='register_url'),
+]
